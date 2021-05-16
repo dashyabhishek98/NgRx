8c87d1b08432f31ec1f9fbabd7b4b7ff4a02bafe
Abhishek <dashyabhishek98@gmail.com>
2021-05-16 18:42:11 +0530

    Create a Add post reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { CounterButtonComponent } from './counter/counter-button/counter-button.
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter/state/counter.reducer';
 import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingmodule } from './app-routing.module';
 import { HeaderComponent } from './shared/components/header/header.component';
@@ -16,6 +16,7 @@ import { PostsListComponent } from './posts/posts-list/posts-list.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
 import { appReducer } from './store/app.state';
+import { AddPostComponent } from './posts/add-post/add-post.component';
 
 @NgModule({
   declarations: [
@@ -26,11 +27,13 @@ import { appReducer } from './store/app.state';
     CustomCounterInputComponent,
     HomeComponent,
     HeaderComponent,
-    PostsListComponent
+    PostsListComponent,
+    AddPostComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingmodule,
     StoreModule.forRoot(appReducer),
     StoreDevtoolsModule.instrument({
---
+++ src/app/posts/add-post/add-post.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-post',
+  templateUrl: './add-post.component.html',
+  styleUrls: ['./add-post.component.css']
+})
+export class AddPostComponent implements OnInit {
+  postForm: FormGroup;
+  constructor() { 
+    this.postForm = new FormGroup({
+      title : new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      description : new FormControl(null, [Validators.required, Validators.minLength(10)])
+    });
+  }
+
+  ngOnInit(): void {
+    this.postForm = new FormGroup({
+      title : new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      description : new FormControl(null, [Validators.required, Validators.minLength(10)])
+    });
+  }
+
+  onAddPost(){
+    if(!this.postForm.valid){
+      return ;
+    }
+  }
+  showErrorDescription() :  string{
+    const descriptionForm = this.postForm.get('description');
+    if(descriptionForm?.touched && !descriptionForm.valid){
+      if(descriptionForm.errors){
+        return 'Description is required';
+      }
+      if(descriptionForm.errors){
+        return 'Description should be minimum 10 character length';
+      }
+    }
+    return "";
+  }
+
+}
